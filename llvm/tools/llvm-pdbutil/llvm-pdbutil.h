c32a63f12933fe19176e4586cfcd1c0358a95281
Ilia Kuklin <ikuklin@accesssoftek.com>
2026-04-15 00:33:11 +0500

    [llvm-pdbutil] Add --dxcontainer option to export DXContainer

--- llvm/tools/llvm-pdbutil/llvm-pdbutil.h
+++ llvm/tools/llvm-pdbutil/llvm-pdbutil.h
@@ -218,6 +218,7 @@ namespace exportstream {
 extern llvm::cl::opt<std::string> OutputFile;
 extern llvm::cl::opt<std::string> Stream;
 extern llvm::cl::opt<bool> ForceName;
+extern llvm::cl::opt<bool> DXContainer;
 } // namespace exportstream
 }
 
