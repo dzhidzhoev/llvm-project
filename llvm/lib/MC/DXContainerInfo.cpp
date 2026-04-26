d720e5260f31cb02aabc6fc1e0616de1e1d8ff28
Vladislav Dzhidzhoev <vdzhidzhoev@accesssoftek.com>
2026-04-26 14:16:56 +0200

    [DirectX] Generate shader source info part in llc
    This change modifies DXContainerGlobals pass and DXILMetadataAnalysis
    to generate source info (SRCI) part in DXContainer, using information
    from dx.source.contants and dx.source.args metadata nodes.
    
    This part is typically generated for debug shader builds.

--- llvm/lib/MC/DXContainerInfo.cpp
+++ llvm/lib/MC/DXContainerInfo.cpp
@@ -9,16 +9,20 @@
 #include "llvm/ADT/SmallString.h"
 #include "llvm/BinaryFormat/DXContainer.h"
 #include "llvm/MC/DXContainerSourceInfo.h"
+#include "llvm/Object/DXContainer.h"
 #include "llvm/Support/Compression.h"
 #include "llvm/Support/EndianStream.h"
 #include "llvm/Support/FormatVariadic.h"
+#include "llvm/Support/MathExtras.h"
 #include "llvm/Support/SwapByteOrder.h"
 #include <type_traits>
 
 using namespace llvm;
 using namespace llvm::mcdxbc;
 
-constexpr static uint64_t DXCONTAINER_STRUCT_ALIGNMENT = 4;
+static uint64_t align(uint64_t Size) {
+  return alignTo(Size, dxbc::DXCONTAINER_STRUCT_ALIGNMENT);
+}
 
 template <typename StructT>
 static void writeStruct(raw_ostream &OS, StructT S) {
@@ -37,12 +41,89 @@ static void writeString(raw_ostream &OS, StringRef S) {
 
 static void writePadding(raw_ostream &OS, uint64_t Prev) {
   uint64_t UnpaddedSize = OS.tell() - Prev;
-  uint64_t Remainder = UnpaddedSize % DXCONTAINER_STRUCT_ALIGNMENT;
-  if (Remainder)
-    OS.write_zeros(DXCONTAINER_STRUCT_ALIGNMENT - Remainder);
+  uint64_t Padding = align(UnpaddedSize) - UnpaddedSize;
+  if (Padding)
+    OS.write_zeros(Padding);
+}
+
+void SourceInfo::compute() {
+  assert(CompressionType && "Compression type must be set.");
+
+  BaseData.Parameters.SectionCount = 3;
+  BaseData.Parameters.Flags = 0;
+
+  BaseData.Names.GenericHeader.Flags = 0;
+  BaseData.Names.GenericHeader.Type =
+      dxbc::SourceInfo::SectionType::SourceNames;
+  BaseData.Names.Parameters.Flags = 0;
+  BaseData.Names.Parameters.Count = FileNamesAndContents.size();
+  BaseData.Names.Entries.clear();
+  BaseData.Names.Entries.reserve(BaseData.Names.Parameters.Count);
+
+  BaseData.Contents.GenericHeader.Flags = 0;
+  BaseData.Contents.GenericHeader.Type =
+      dxbc::SourceInfo::SectionType::SourceContents;
+  BaseData.Contents.Parameters.Flags = 0;
+  BaseData.Contents.Parameters.Type = *CompressionType;
+  BaseData.Contents.Parameters.Count = FileNamesAndContents.size();
+  BaseData.Contents.Entries.clear();
+  BaseData.Contents.Entries.reserve(BaseData.Contents.Parameters.Count);
+
+  size_t NameEntriesSizeInBytes = 0;
+  size_t ContentEntriesSizeInBytes = 0;
+  for (const auto &NameContent : FileNamesAndContents) {
+    object::DirectX::SourceInfo::SourceNames::Entry NameEntry;
+    NameEntry.FileName = NameContent.first;
+    NameEntry.Parameters.ContentSizeInBytes = NameContent.second.size() + 1;
+    NameEntry.Parameters.NameSizeInBytes = NameEntry.FileName.size() + 1;
+    NameEntry.Parameters.Flags = 0;
+    NameEntry.Parameters.AlignedSizeInBytes =
+        align(NameEntry.Parameters.NameSizeInBytes +
+              sizeof(dxbc::SourceInfo::Names::Entry));
+    NameEntriesSizeInBytes += NameEntry.Parameters.AlignedSizeInBytes;
+
+    object::DirectX::SourceInfo::SourceContents::Entry ContentEntry;
+    ContentEntry.FileContent = NameContent.second.str();
+    ContentEntry.Parameters.ContentSizeInBytes =
+        NameEntry.Parameters.ContentSizeInBytes;
+    ContentEntry.Parameters.Flags = 0;
+    ContentEntry.Parameters.AlignedSizeInBytes =
+        align(ContentEntry.Parameters.ContentSizeInBytes +
+              sizeof(dxbc::SourceInfo::Contents::Entry));
+    ContentEntriesSizeInBytes += ContentEntry.Parameters.AlignedSizeInBytes;
+
+    BaseData.Names.Entries.emplace_back(std::move(NameEntry));
+    BaseData.Contents.Entries.push_back(std::move(ContentEntry));
+  }
+  BaseData.Names.Parameters.EntriesSizeInBytes = align(NameEntriesSizeInBytes);
+  BaseData.Names.GenericHeader.updateSize(
+      BaseData.Names.Parameters.EntriesSizeInBytes +
+      sizeof(dxbc::SourceInfo::Names::HeaderOnDisk));
+  BaseData.Contents.Parameters.UncompressedEntriesSizeInBytes =
+      align(ContentEntriesSizeInBytes);
+
+  BaseData.Args.GenericHeader.Flags = 0;
+  BaseData.Args.GenericHeader.Type = dxbc::SourceInfo::SectionType::Args;
+  BaseData.Args.Parameters.Flags = 0;
+  BaseData.Args.Parameters.Count = Args.size();
+  BaseData.Args.Args.clear();
+  BaseData.Args.Args.reserve(BaseData.Args.Parameters.Count);
+  size_t ArgEntriesSizeInBytes = 0;
+  for (StringRef Arg : Args) {
+    BaseData.Args.Args.emplace_back(Arg, "");
+    // Null-terminated argument name and empty null-terminated argument value.
+    ArgEntriesSizeInBytes += Arg.size() + 1 + 1;
+  }
+  BaseData.Args.Parameters.SizeInBytes = ArgEntriesSizeInBytes;
+  BaseData.Args.GenericHeader.updateSize(
+      BaseData.Args.Parameters.SizeInBytes +
+      sizeof(dxbc::SourceInfo::Args::Header));
 }
 
 void SourceInfo::finalize() {
+  if (Override)
+    compute();
+
   IsFinalized = true;
 
   // Compress Contents right here, to calculate compressed size.
@@ -73,10 +154,16 @@ void SourceInfo::finalize() {
     raw_svector_ostream OS(CompressedContents);
     OS.write(reinterpret_cast<char *>(CompressedData.data()),
              CompressedData.size());
+    if (Override)
+      BaseData.Contents.Parameters.EntriesSizeInBytes =
+          CompressedContents.size();
     break;
   }
   case dxbc::SourceInfo::Contents::CompressionType::None: {
     CompressedContents = std::move(Data);
+    if (Override)
+      BaseData.Contents.Parameters.EntriesSizeInBytes =
+          align(CompressedContents.size());
     break;
   }
   }
@@ -87,6 +174,19 @@ void SourceInfo::finalize() {
                 "doesn't match the actual compressed size ({1})",
                 BaseData.Contents.Parameters.EntriesSizeInBytes,
                 CompressedContents.size()));
+
+  if (Override) {
+    BaseData.Contents.Parameters.AlignedSizeInBytes =
+        align(BaseData.Contents.Parameters.EntriesSizeInBytes +
+              sizeof(dxbc::SourceInfo::Contents::Header));
+    BaseData.Contents.GenericHeader.updateSize(
+        BaseData.Contents.Parameters.AlignedSizeInBytes);
+    BaseData.Parameters.AlignedSizeInBytes =
+        align(sizeof(dxbc::SourceInfo::Header) +
+              BaseData.Names.GenericHeader.AlignedSizeInBytes +
+              BaseData.Contents.GenericHeader.AlignedSizeInBytes +
+              BaseData.Args.GenericHeader.AlignedSizeInBytes);
+  }
 }
 
 void SourceInfo::write(raw_ostream &OS) const {
