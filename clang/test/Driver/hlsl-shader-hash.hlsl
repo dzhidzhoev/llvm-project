7a5a4cc35b607a9996546323b2d47236d4fd33a2
Ilia Kuklin <ikuklin@accesssoftek.com>
2026-05-22 20:58:40 +0500

    [Driver][DirectX] Add -Zsb flag and fix/add flag tests

---
+++ clang/test/Driver/hlsl-shader-hash.hlsl
@@ -0,0 +1,7 @@
+// REQUIRES: directx-registered-target
+
+// RUN: %clang_dxc -T lib_6_7 -Zss -### %s 2>&1 | FileCheck %s --check-prefix=CHECK-ZSS
+// CHECK-ZSS: -dx-Zss
+
+// RUN: not %clang_dxc -T lib_6_7 -Zss -Zsb -### %s 2>&1 | FileCheck %s --check-prefix=CHECK-BOTH
+// CHECK-BOTH: cannot specify both /Zss and /Zsb
