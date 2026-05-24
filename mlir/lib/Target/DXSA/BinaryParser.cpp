d7b8e520b8ee906e626a275c8bdcef37df044957
Vladimir Shiryaev <tagolog@users.noreply.github.com>
2026-05-24 11:07:30 -0700

    [mlir][dxsa] Add dcl_output_siv instruction
    Example:
      dxsa.dcl_output_siv <type = output, components = 4, mask = <x, y, z, w>, index = [0]>, <position>
    
    Signed-off-by: Vladimir Shiryaev <tagolog@users.noreply.github.com>

--- mlir/lib/Target/DXSA/BinaryParser.cpp
+++ mlir/lib/Target/DXSA/BinaryParser.cpp
@@ -647,6 +647,12 @@ public:
     return dxsa::DclOutputSgv::create(builder, loc, operand, nameAttr);
   }
 
+  Instruction buildDclOutputSiv(dxsa::InlineOperandAttr operand,
+                                dxsa::SystemValueName name, Location loc) {
+    auto nameAttr = dxsa::SystemValueNameAttr::get(builder.getContext(), name);
+    return dxsa::DclOutputSiv::create(builder, loc, operand, nameAttr);
+  }
+
 private:
   MLIRContext *context;
   ModuleOp module;
@@ -1202,6 +1208,14 @@ public:
     return builder.buildDclOutputSgv(*operand, *name, loc);
   }
 
+  FailureOr<Instruction> parseDclOutputSiv(Location loc) {
+    auto operand = parseInlineOperand();
+    FAILURE_IF_FAILED(operand);
+    auto name = parseSystemValueName(getLocation());
+    FAILURE_IF_FAILED(name);
+    return builder.buildDclOutputSiv(*operand, *name, loc);
+  }
+
   OptionalParseResult parseDclInstruction(uint32_t opcodeToken, Location loc,
                                           Instruction &out) {
     FailureOr<Instruction> result;
@@ -1253,6 +1267,9 @@ public:
       break;
     case D3D10_SB_OPCODE_DCL_OUTPUT_SGV:
       result = parseDclOutputSgv(loc);
+      break;
+    case D3D10_SB_OPCODE_DCL_OUTPUT_SIV:
+      result = parseDclOutputSiv(loc);
       break;
     default:
       return std::nullopt;
