faeeeec971358c2c27b9d752c72e87d578b257f3
Ilia Kuklin <ikuklin@accesssoftek.com>
2026-06-17 21:19:13 +0500

    Use Twine in error message

--- llvm/lib/Target/DirectX/DXContainerPDB.cpp
+++ llvm/lib/Target/DirectX/DXContainerPDB.cpp
@@ -148,8 +148,8 @@ bool DXContainerPDB::runOnModule(Module &M) {
   auto BypassSandbox = sys::sandbox::scopedDisable();
   codeview::GUID IgnoredOutGuid;
   if (Error Err = Builder.commit(DebugFileName, &IgnoredOutGuid))
-    reportFatalUsageError(formatv("Couldn't write to PDB file: {0}",
-                                  toString(std::move(Err)).c_str()));
+    reportFatalUsageError("Couldn't write to PDB file: " +
+                          Twine(toString(std::move(Err))));
 
   reset();
 
